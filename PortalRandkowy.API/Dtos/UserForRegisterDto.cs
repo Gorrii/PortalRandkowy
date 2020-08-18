96b0ab41e264a241a4d9ebafaac1dc9e7aa88735
Aleksander Golek <aleksander.golek.ext@siemens.com>
2020-08-18 18:13:40 +0200

    Dodanie validacji do UserForRegisterDTO

--- PortalRandkowy.API/Dtos/UserForRegisterDto.cs
+++ PortalRandkowy.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortalRandkowy.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="Nazwa Uzytkownika jest wymagana")]
         public string UserName { get; set; }  
+        
+        [Required(ErrorMessage="haslo  jest wymagana")]
+        [StringLength(12,MinimumLength=6, ErrorMessage="min 6 max 12")]
         public string Password { get; set; }  
     }
 }
